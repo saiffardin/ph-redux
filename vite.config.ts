de63baa20de16a2c3ee29124cb88f408529fe93f
Saif Chowdhury <saiffardin@gmail.com>
2025-07-01 02:25:39 +0600

    add path alias resolver

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import checker from "vite-plugin-checker";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), checker({ typescript: true })],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
