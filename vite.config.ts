421e617b4e641a7fce16be7682b7f79457ec8440
Saif Chowdhury <saiffardin@gmail.com>
2025-07-01 02:11:51 +0600

    add tailwind || prettier || vite-checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss(), checker({ typescript: true })],
+});
