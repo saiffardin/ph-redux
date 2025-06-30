32d715882621619f44dcba4d2b1c7a911f17a674
Saif Chowdhury <saiffardin@gmail.com>
2025-07-01 02:40:03 +0600

    shadCN added

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 const App = () => {
   return (
     <div className="bg-gray-800 h-screen">
       <h1 className="text-3xl text-white">Hello World !!</h1>
+
+      <Button>Test</Button>
     </div>
   );
 };
